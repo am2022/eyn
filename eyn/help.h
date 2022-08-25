c76e6d68f999e71803c8ab7d3e36ebc46882ce35
am <am2022a2@gmail.com>
2022-08-25 11:45:42 +0430

    make help.h file

---
+++ eyn/help.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void argv_help(){
+    printf("-i or -fname_in for input file\n");
+    printf("-o or -fname_out for out put file\n");
+    printf("-k1 or -key1 for password1\n");
+    printf("-k2 or -key2 for password2\n");
+    printf("--help for help\n");
+}
