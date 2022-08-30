5f971b684669f71ac71093a14fe5ea0dcf59bc43
am <am2022a2@gmail.com>
2022-08-30 07:26:20 +0430

    update help.h file

--- eyn/help.h
+++ eyn/help.h
@@ -5,5 +5,8 @@ void argv_help(){
     printf("-o or -fname_out for out put file\n");
     printf("-k1 or -key1 for password1\n");
     printf("-k2 or -key2 for password2\n");
+    printf("-txt_o for save passwords in txt format\n");
+    printf("-md_o for save passwords in md format\n");
+    printf("-html_o for save passwords in html format\n");
     printf("--help or -h or -help for help\n");
 }
