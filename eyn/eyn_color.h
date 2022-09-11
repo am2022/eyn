f13174f98e0aeb4369144c5fd87ddbbe8c8bdb38
am <am2022a2@gmail.com>
2022-09-11 21:38:47 +0430

    create eyn_color.h and make main_color function

---
+++ eyn/eyn_color.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main_color(char *os){
+    char bg;
+    char fg;
+
+    printf("color menu:\n")
+    printf("0 = Black       8 = Gray\n");
+    printf("1 = Blue        9 = Light Blue\n");
+    printf("2 = Green       A = Light Green\n");
+    printf("3 = Aqua        B = Light Aqua\n");
+    printf("4 = Red         C = Light Red\n");
+    printf("5 = Purple      D = Light Purple\n");
+    printf("6 = Yellow      E = Light Yellow\n");
+    printf("7 = White       F = Bright White\n");
+
+    printf("enter background color:");
+    scanf("%c", &bg);
+
+    printf("enter foreground color:");
+    scanf("%c", &fg);
+}
