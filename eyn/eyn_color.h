69a18576ed0e50f52f03c3088780fd5373bb3fdc
am <am2022a2@gmail.com>
2022-09-13 08:37:34 +0430

    add if for windows platform in main_color function

--- eyn/eyn_color.h
+++ eyn/eyn_color.h
@@ -9,52 +9,54 @@ void set_color(char bg, char fg){
 }
 
 void main_color(char *os){
-    char bg;
-    char fg;
-    char colors[] = "0123456789ABCDEF";
+    if(! strcmp(os, "win")){
+        char bg;
+        char fg;
+        char colors[] = "0123456789ABCDEF";
 
-    printf("color menu:\n");
-    printf("0 = Black       8 = Gray\n");
-    printf("1 = Blue        9 = Light Blue\n");
-    printf("2 = Green       A = Light Green\n");
-    printf("3 = Aqua        B = Light Aqua\n");
-    printf("4 = Red         C = Light Red\n");
-    printf("5 = Purple      D = Light Purple\n");
-    printf("6 = Yellow      E = Light Yellow\n");
-    printf("7 = White       F = Bright White\n");
+        printf("color menu:\n");
+        printf("0 = Black       8 = Gray\n");
+        printf("1 = Blue        9 = Light Blue\n");
+        printf("2 = Green       A = Light Green\n");
+        printf("3 = Aqua        B = Light Aqua\n");
+        printf("4 = Red         C = Light Red\n");
+        printf("5 = Purple      D = Light Purple\n");
+        printf("6 = Yellow      E = Light Yellow\n");
+        printf("7 = White       F = Bright White\n");
 
-    printf("enter background color:");
-    scanf(" %c", &bg);
+        printf("enter background color:");
+        scanf(" %c", &bg);
 
-    printf("enter foreground color:");
-    scanf(" %c", &fg);
+        printf("enter foreground color:");
+        scanf(" %c", &fg);
 
-    int flag = 0;
-    int flag2 = 0;
+        int flag = 0;
+        int flag2 = 0;
 
-    for(int i = 0;i <= sizeof(colors);i++){
-        if(bg == colors[i]){
-            flag = 1;
-            break;
+        for(int i = 0;i <= sizeof(colors);i++){
+            if(bg == colors[i]){
+                flag = 1;
+                break;
+            }
         }
-    }
 
-    if(flag == 0){
-        printf("incorrect input!\n");
-        return;
-    }
-
-    for(int i = 0;i <= sizeof(colors);i++){
-        if(fg == colors[i]){
-            flag2 = 1;
-            break;
+        if(flag == 0){
+            printf("incorrect input!\n");
+            return;
         }
-    }
 
-    if(flag2 == 0){
-        printf("incorrect input!\n");
-        return;
-    }
+        for(int i = 0;i <= sizeof(colors);i++){
+            if(fg == colors[i]){
+                flag2 = 1;
+                break;
+            }
+        }
 
-    set_color(bg, fg);
+        if(flag2 == 0){
+            printf("incorrect input!\n");
+            return;
+        }
+
+        set_color(bg, fg);
+    }
 }
