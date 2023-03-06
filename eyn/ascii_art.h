9cfb1f3982c0f4cc786d9f39c62512ad7cf493b4
am <am2022a2@gmail.com>
2023-03-06 21:25:52 +0330

    print eyn in ascii_art function

--- eyn/ascii_art.h
+++ eyn/ascii_art.h
@@ -2,4 +2,10 @@
 using namespace std;
 
 void ascii_art(){
+    cout<<"   ___ _   _ _ __\n";
+    cout<<"  / _ \\ | | | '_ \\\n";
+    cout<<" |  __/ |_| | | | |\n";
+    cout<<"  \\___|\\__, |_| |_|\n";
+    cout<<"        __/ |\n";
+    cout<<"       |___/\n";
 }
